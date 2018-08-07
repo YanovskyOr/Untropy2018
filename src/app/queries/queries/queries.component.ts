6b844ee22d81e7d59bfc31c2cf30b76957f4988a
Lilia <lilia.misotchenko@gmail.com>
2018-08-07 16:54:53 +0300

    add queryb

--- src/app/queries/queries/queries.component.ts
+++ src/app/queries/queries/queries.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Querya } from '../querya';
+import { Queryb } from '../queryb';
 
 
 @Component({
@@ -10,22 +11,34 @@ import { Querya } from '../querya';
 })
 export class QueriesComponent implements OnInit {
 
-  readonly url = "http://uvo10ntf2e964aukvam.vm.cld.sr/servers/querya";
+  readonly url = "http://uvo10ntf2e964aukvam.vm.cld.sr/servers";
   status =["critical","Warning","ok"];
   time=[5,10,20,30,100000];
 
-  query =new Querya("",null);
+  querya =new Querya("",null);
+  queryb =new Queryb("",null,null,null);
   submitted=false;
   constructor(private http: HttpClient) { };
 
   getQuerya(status: string, time: number){//move to maybe service?
     
-    let data = "/"+ status +"/"+time;
+    let data = "/querya"+"/"+ status +"/"+time;
     this.submitted=true;
     return this.http.get(this.url+data).subscribe((data1:any[])=>{console.log(data1)});
-    
   }
-  get diagnostic() { return JSON.stringify(this.query); }
+
+
+  getQueryb(name: string, numofok: number,numofwarn: number,numofcrit: number){//move to maybe service?
+    
+    let data = "/queryb"+"/"+ name +"/"+numofok+"/"+numofwarn+"/"+numofcrit;
+    this.submitted=true;
+    return this.http.get(this.url+data).subscribe((data1:any[])=>{console.log(data1)});
+  }
+
+
+
+
+  get diagnostic() { return JSON.stringify(this.queryb); }
 
   
 
---
+++ src/app/queries/queryb.ts
@@ -0,0 +1,8 @@
+export class Queryb {
+    constructor(
+        public name: string,
+        public numofok: number,
+        public numofwarn: number,
+        public numofcrit: number
+        )   {}
+}
